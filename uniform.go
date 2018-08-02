93edaf83f2c7c4c452d88eaaf73c06c16fb07d46
Louis Thibault <l.thibault@sentimens.com>
2018-08-02 14:41:59 +0100

    Add jitterer that uses uniform distribution

---
+++ uniform.go
@@ -0,0 +1,33 @@
+package jitter
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Uniform distribution
+type Uniform struct {
+	Source *rand.Rand
+	Limit  time.Duration
+}
+
+// Jitter the duration by drawing from a uniform distribution
+func (u Uniform) Jitter(d time.Duration) time.Duration {
+	f := rand.Int63n
+	if u.Source != nil {
+		f = u.Source.Int63n
+	}
+
+	sign := rand.Intn
+	if u.Source != nil {
+		sign = u.Source.Intn
+	}
+
+	samp := time.Duration(f(int64(u.Limit)))
+
+	if sign(1) == 0 {
+		return d - samp
+	}
+
+	return d + samp
+}
