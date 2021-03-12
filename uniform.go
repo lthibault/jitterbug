7edacbc5906af8004ee8c17b90fa1fe745dcecb9
hfacey <35274579+hfacey@users.noreply.github.com>
2021-03-12 10:14:35 -0800

    Unbias the uniform distribution (#5)
    Fix https://github.com/lthibault/jitterbug/issues/4
    * de-bias the uniform distribution
    * throw panic if duration <= min

--- uniform.go
+++ uniform.go
@@ -12,12 +12,16 @@ type Uniform struct {
 }
 
 // Jitter the duration by drawing from a uniform distribution
+// over the range [Min, d). Panics if d <= Min
 func (u Uniform) Jitter(d time.Duration) time.Duration {
 	drawUniform := rand.Int63n
 	if u.Source != nil {
 		drawUniform = u.Source.Int63n
 	}
 
-	d = time.Duration(drawUniform(int64(d)))
-	return max(d, u.Min)
+	delta := d - u.Min
+	if delta <= 0 {
+		panic("duration must exceed min")
+	}
+	return u.Min + time.Duration(drawUniform(int64(delta)))
 }
