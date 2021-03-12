4baa6648e462cba50ba6e913ca586ca8259d5c0c
Louis Thibault <l.thibault@sentimens.com>
2021-03-12 13:41:36 -0500

    Clarify behavior of distributions with respect to how jitter is applied.

--- uniform.go
+++ uniform.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-// Uniform distribution
+// Uniform distribution.
 type Uniform struct {
 	Source *rand.Rand
 	Min    time.Duration
 }
 
-// Jitter the duration by drawing from a uniform distribution
-// over the range [Min, d). Panics if d <= Min
+// Jitter the duration uniformly over the range [Min, d).
+// The zero-value is ready to use.  Panics if d <= Min.
 func (u Uniform) Jitter(d time.Duration) time.Duration {
 	drawUniform := rand.Int63n
 	if u.Source != nil {
